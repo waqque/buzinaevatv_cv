9a6a95378eb526b16c8a63829f88e22aec853c06
waqque <tanos_hanos@mail.ru>
2025-04-19 23:48:45 +0800

    figurki done

---
+++ figures_and_colors/main.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.measure import label, regionprops
+img = cv2.imread("balls_and_rects.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+binary = cv2.threshold(gray, 25, 255, cv2.THRESH_BINARY)[1]
+labeled = label(binary)
+circles = 0
+rect = 0
+regions = regionprops(labeled, intensity_image=gray)
+for region in regions:
+    ecc = region.eccentricity  
+    if ecc < 0.7 :
+            circles += 1
+    else: 
+            rect += 1
+print(f"Всего фигур: {len(regions)}")
+print(f"Всего кругов: {circles}")
+print(f"Всего прямоугольников: {rect}")
