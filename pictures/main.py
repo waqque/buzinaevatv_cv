398013ab008be1a7321f13f0ba54ebae54e9a2ce
waqque <tanos_hanos@mail.ru>
2025-05-19 10:42:28 +0800

    Update main.py

--- pictures/main.py
+++ pictures/main.py
@@ -1,20 +1,22 @@
 import cv2
-
-video = cv2.VideoCapture("output (1).avi")
-
-cnt_img = 0
+import numpy as np
+import os
+vid = cv2.VideoCapture("output (1).avi")
+# output_dir = 'out'
+# os.makedirs(output_dir, exist_ok=True)
+# frame_count = 0
+count = 0
 while True:
-    ret, frame = video.read()
+    ret, frame = vid.read()
     if not ret:
         break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) == 6:
-        cv2.imshow("Gray", thresh)
-        cnt_img += 1
-    if cv2.waitKey(2) & 0xFF == ord('q'):
-        break
-video.release()
-cv2.destroyAllWindows()
-print(cnt_img)
+    else:
+        avg_color = np.average(np.average(np.average(frame, axis=0), axis=0))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 7 and avg_color > 30:
+            # cv2.imwrite(f'{output_dir}/frame_{frame_count}_with_count.png', frame)
+            # frame_count += 1
+            count += 1
+print(count)
